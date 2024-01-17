d3a5af161e7c1b624092d17f591ed9b6f713ec42
Blake Netzeband <blakenetzeband@discover.com>
2024-01-17 08:50:04 -0700

    readme. update folder structure

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Playwright on LambdaTest cloud
+# Playwright on LambdaTest Cloud
+
+This monorepo includes 5 workspaces:
+
+* test-js
+* test-ts
+* jest-js
+* cucumber-js
+* ct (this uses @playwright/experimental-ct-react)
+
+Each workspace has a test command that runs on LambdaCloud. Some workspaces have a `test:local` command that will only run locally.
+
+This project was cloned from [LambdaTest's Playwright Sample Repo](https://github.com/LambdaTest/playwright-sample/). Everything in the `archive` directory seemed unrelated to our project.
 
--- packages/test-ts/playwright.config.ts
+++ packages/test-ts/playwright.config.ts
@@ -48,30 +48,30 @@ const config: PlaywrightTestConfig = {
             viewport: { width: 1920, height: 1080 },
           },
         },
-        // {
-        //   name: "chrome:latest:Windows 11@lambdatest",
-        //   use: {
-        //     viewport: { width: 1280, height: 720 },
-        //   },
-        // },
-        // {
-        //   name: "MicrosoftEdge:latest:MacOS Ventura@lambdatest",
-        //   use: {
-        //     ...devices["iPhone 12 Pro Max"],
-        //   },
-        // },
-        // {
-        //   name: "pw-firefox:latest:Windows 11@lambdatest",
-        //   use: {
-        //     viewport: { width: 1280, height: 720 },
-        //   },
-        // },
-        // {
-        //   name: "pw-webkit:latest:Windows 10@lambdatest",
-        //   use: {
-        //     viewport: { width: 1920, height: 1080 },
-        //   },
-        // },
+        {
+          name: "chrome:latest:Windows 11@lambdatest",
+          use: {
+            viewport: { width: 1280, height: 720 },
+          },
+        },
+        {
+          name: "MicrosoftEdge:latest:MacOS Ventura@lambdatest",
+          use: {
+            ...devices["iPhone 12 Pro Max"],
+          },
+        },
+        {
+          name: "pw-firefox:latest:Windows 11@lambdatest",
+          use: {
+            viewport: { width: 1280, height: 720 },
+          },
+        },
+        {
+          name: "pw-webkit:latest:Windows 10@lambdatest",
+          use: {
+            viewport: { width: 1920, height: 1080 },
+          },
+        },
       ],
 };
 
